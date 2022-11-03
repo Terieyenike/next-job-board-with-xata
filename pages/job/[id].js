627f28b4ac889ba8ecf00492e565af7ed100f3f2
teri eyenike <teyenike1@gmail.com>
2022-11-03 15:35:35 +0100

    Add: job board app

---
+++ pages/job/[id].js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Job = () => {
+  return (
+    <div>
+      <p>work at adyen</p>
+      <Link href='/'>Go back home</Link>
+    </div>
+  );
+};
+
+export default Job;
